b2ef7df513e58f565e613fae6866eaff6b431b33
Dev_Pelz <pelzfx1@gmail.com>
2023-09-10 18:30:03 +0100

    add: scripts/deployTokenB.ts

---
+++ scripts/deployTokenB.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+
+async function main() {
+
+  const TokenB = await ethers.deployContract("TokenB", []);
+
+  await TokenB.waitForDeployment();
+
+  console.log(
+`Token deployed to ${TokenB.target}`
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
